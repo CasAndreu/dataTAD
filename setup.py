248f9f001ed859768111f03effe8b459b6e2daba
CasAndreu <acasas2@uw.edu>
2016-11-15 12:04:00 -0800

    updating diff module

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def readme():
         return f.read()
 
 setup(name='dataTAD',
-      version='0.1',
+      version='0.31',
       description='A module with sample data to learn Text as Data for Social Science Research',
       url='http://andreucasas.com/text_as_data/',
       author='Andreu Casas',
       author_email='acasas2@uw.edu',
       license='MIT',
-      packages=['rlda'],
-      install_requires=[],
+      packages=['dataTAD'],
+      install_requires=['re', 'json'],
       zip_safe=False,
       include_package_data=True)
